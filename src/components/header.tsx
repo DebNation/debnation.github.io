a0edc2d5ca7b0eaeaae4d247ae1c485a1485dab0
DebXD <debiprasadxd@protonmail.com>
2023-04-18 20:23:12 +0530

    use poppins font in header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,10 +4,10 @@ interface HeaderPropTypes {
 }
 const Header = () =>{
     return (
-        <div className="bg-gray-800 text-white p-5   shadow-lg">
-            <div className="text-xl font-['JetBrains_Mono']">DEBxD</div>
-        </div>
-    )
+      <div className="bg-gray-800 text-white p-5 shadow-lg">
+        <div className="text-xl font-['Poppins']">DEBxD</div>
+      </div>
+    );
 }
 
 export default Header;
