70233ed4bb4616bb1654c75c10faddb3ccfc4ccb
DebXD <debiprasadxd@protonmail.com>
2023-04-19 19:05:37 +0530

    some modification, font changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,26 +3,40 @@ import { MdDarkMode, MdLightMode } from "react-icons/md";
 import Link from "next/link";
 import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 import { FaTelegramPlane } from "react-icons/fa";
-import { JetBrains_Mono, Teko, Mr_Dafoe, Fira_Code } from "next/font/google";
+import {
+  JetBrains_Mono,
+  Teko,
+  Mr_Dafoe,
+  Poppins,
+  Shadows_Into_Light,
+  Caveat,
+} from "next/font/google";
 import Image from "next/image";
 import satoru from "../../public/satoru.jpeg";
+import laptop from "../../public/laptop.png";
 import { useState } from "react";
 
 const jetbrainsMono = JetBrains_Mono({ subsets: ["latin"], weight: "400" });
 const teko = Teko({ subsets: ["latin"], weight: "400" });
 const arizonia = Mr_Dafoe({ subsets: ["latin"], weight: "400" });
-const firaCode = Fira_Code({ subsets: ["latin"], weight: "400" });
+const poppins = Poppins({ subsets: ["latin"], weight: "400" });
+const caveat = Caveat({ subsets: ["latin"], weight: "400" });
+
+const shadowsIntoLight = Shadows_Into_Light({
+  subsets: ["latin"],
+  weight: "400",
+});
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(false);
   return (
     <div className={darkMode ? "dark" : ""}>
       <Header />
-      <main className="px-10 bg-white dark:bg-black">
+      <main className="px-10 bg-gray-100 dark:bg-gray-800">
         <section className="min-h-screen">
           <nav className="py-10 mb-10 flex justify-between">
             <h1
-              className={`text-4xl ${arizonia.className} text-violet-600 cursor-pointer`}
+              className={`text-4xl ${arizonia.className} text-violet-600 dark:text-violet-400 cursor-pointer`}
             >
               <Link href={"/"}>Debxd</Link>
             </h1>
@@ -47,7 +61,7 @@ export default function Home() {
               <li>
                 <Link
                   href={"/"}
-                  className="bg-gradient-to-r  from-cyan-500 to-teal-500 py-2 px-4 text-white rounded-md ml-8"
+                  className={`${poppins.className} bg-gradient-to-r  from-cyan-500 to-teal-500 hover:bg-gradient-to-r hover:from-cyan-700 hover:to-teal-700 py-2 px-4 text-white rounded-md ml-8`}
                 >
                   Projects
                 </Link>
@@ -65,18 +79,19 @@ export default function Home() {
           </div>
           <div className="text-center px-10">
             <h2
-              className={`text-5xl py-2 text-teal-600 font-medium ${teko.className} `}
+              className={`text-5xl py-2 text-teal-600 dark:text-teal-500 tracking-wide  ${teko.className} `}
             >
               Debiprasad Das
             </h2>
             <h3
               className={`text-2xl py-2 text-gray-900 dark:text-gray-200 ${jetbrainsMono.className}`}
             >
-              Web Developer, FOSS & Linux Enthuasiast
+              Developer, FOSS & Linux Enthuasiast
             </h3>
-            <p className=" py-5 leading-8 text-gray-800 dark:text-gray-300 text-lg">
-              Freeelancer providing services for clients to use like react and
-              etc. Follow me to get updates.
+            <p
+              className={`${caveat.className} py-5 leading-8 text-gray-800 dark:text-gray-300 text-2xl`}
+            >
+              I like to work on React with typescript, also on Flask.
             </p>
           </div>
           <div className="text-4xl flex justify-center text-gray-600 dark:text-gray-400 gap-16 py-3">
